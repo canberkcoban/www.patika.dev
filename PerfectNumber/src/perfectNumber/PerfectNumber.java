b98ae49a8d4b54ec21c54de276e513a899fce903
Canberk Coban <cobancanberk@gmail.com>
2023-03-15 16:33:04 +0300

    Perfect Number

---
+++ PerfectNumber/src/perfectNumber/PerfectNumber.java
@@ -0,0 +1,28 @@
+package perfectNumber;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		int number, total = 0;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the number: ");
+		number = input.nextInt();
+		
+		 for (int i = 1; i < number; i++) {
+	            if (number % i == 0) {
+	                total += i;
+	            }
+	        }
+
+	        if (total == number) {
+	            System.out.println(number + " is a perfect number.");
+	            
+	        }
+	        else {
+	            System.out.println(number + " is not a perfect number.");
+	        }
+	}
+}
